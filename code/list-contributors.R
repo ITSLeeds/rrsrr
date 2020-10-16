83c4c9bfcf6e5589c78d8522f351b627db2bf092
Robin Lovelace <rob00x@gmail.com>
2020-10-16 09:55:39 +0100

    Update with contributors section

---
+++ code/list-contributors.R
@@ -0,0 +1,19 @@
+# List contributors
+# source: https://github.com/Robinlovelace/geocompr/blob/master/code/list-contributors.R
+
+# attach packages
+library(gh)
+library(tidyverse)
+
+#**********************************************************
+# 2 CONTRIBUTOR LIST---------------------------------------
+#**********************************************************
+
+# git has to be in PATH
+out_json = gh::gh(endpoint = "/repos/saferactive/rrsrr/contributors", .limit = "Inf")
+link = vapply(out_json, "[[", FUN.VALUE = "", "html_url")
+name = gsub(pattern = "https://github.com/", "", link)
+commits = paste0("https://github.com/saferactive/rrsrr/commits?author=", name)
+out_df = tibble(name, link)
+# remove book authors
+filter(out_df, !grepl("robin", name, TRUE))
